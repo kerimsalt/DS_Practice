d24db442bafeecedf223bb02d54f1b524bdef46a
kerimsalt <kerim.saltoglu@mail.utoronto.ca>
2025-04-10 09:41:35 -0400

    added some statistics about the survivors.

--- data_processing_practice.py
+++ data_processing_practice.py
@@ -1,8 +1,8 @@
 import seaborn as sns
-import numpy as np
 df = sns.load_dataset('titanic')
 # print(df.head())
 # print(df.iloc[0, :5])  # Display the first 5 rows and first 5 columns
+# Assume that the df is global and already loaded
 
 
 def average_age_of_survivors():
@@ -17,8 +17,20 @@ def average_age_of_survivors():
 def survival_rate():
     total = len(df)
     n_survivors = len(df[df["survived"] == 1])
-    print(n_survivors/total * 100)
+    rate = round(n_survivors/total * 100)
+    print(f"Survival rate: {rate}%")
 
 
-average_age_of_survivors()
-sr()
+def gender_distribution_of_survivors():
+    survivors = df[df['survived'] == 1]
+    n_males = len(survivors[survivors['sex'] == 'male'])
+    n_females = len(survivors[survivors['sex'] == 'female'])
+    male_rate = round(n_males / len(survivors) * 100, 2)
+    female_rate = round(n_females / len(survivors) * 100, 2)
+    print(f"{male_rate}% of the survivors were male")
+    print(f"{female_rate}% of the survivors were female")
+
+
+# average_age_of_survivors()
+# survival_rate()
+gender_distribution_of_survivors()
