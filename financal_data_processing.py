79f7b32250a8a1adda3af670e31ef980132a0353
kerimsalt <kerim.saltoglu@mail.utoronto.ca>
2025-04-12 06:11:23 -0400

    Min max normalization and z-score normalization.

--- financal_data_processing.py
+++ financal_data_processing.py
@@ -9,11 +9,25 @@ def z_score_normalization(df):
     Apply Z-score normalization to numeric columns in the DataFrame.
     Non-numeric columns will be left unchanged.
     """
+    print("before")
+    print(df["closing_price"])
     df_numeric = df.select_dtypes(include=['float64', 'int64'])
     df_scaled = (df_numeric - df_numeric.mean()) / df_numeric.std()
-
+    print("after")
+    print(df_scaled["closing_price"])
     # To keep original non-numeric columns:
     return df_scaled.join(df.drop(columns=df_numeric.columns))
+
+
+def min_max_normalization(df):
+    print("before")
+    print(df["closing_price"])
+    df_numeric = df.select_dtypes(include=['float64', 'int64'])
+    df_scaled = (df_numeric - df_numeric.min()) / (df_numeric.max() -
+                                                   df_numeric.min())
+    df_scaled = df_scaled.join(df.drop(columns=df_numeric.columns))
+    print("after")
+    print(df_scaled["closing_price"])
 
 
 def remove_low_variance_columns(df, threshold=0.01):
@@ -38,4 +52,9 @@ def remove_low_variance_columns(df, threshold=0.01):
     return df.drop(columns=low_variance_cols)
 
 
+print("remove low variance")
 remove_low_variance_columns(df, 0.01)
+print("z-score normalization")
+z_score_normalization(df)
+print("min max normalization")
+min_max_normalization(df)
